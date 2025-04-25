3607888774bce63b69b615d2eeeaae47c44332d4
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2025-04-25 17:00:56 +0530

    Created React query custom hook to implement parallel querie

--- src/components/commons/PageNotFound.jsx
+++ src/components/commons/PageNotFound.jsx
@@ -3,7 +3,7 @@ import { NoData } from "neetoui";
 const PageNotFound = () => (
   <div className="absolute left-1/3 top-1/3">
     <NoData
-      title="The Page You are looking fir cant be found"
+      title="The page you are looking for cant be found"
       primaryButtonProps={{
         label: "Back to home",
         className: "bg-neutral-800 hover:bg-neutral-950",
